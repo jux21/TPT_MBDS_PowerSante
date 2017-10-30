e1fd9907a4fe296ff8ef0518829b6c9b5688a3fe
Jux <julien2105@gmail.com>
2017-10-30 01:11:48 +0100

    Benchmark top 10 items from mongo : Spark SQL with sql string requests VS Spark SQL with Dataframe functions.

--- tptscala/src/main/scala/mbds/tpt/App.scala
+++ tptscala/src/main/scala/mbds/tpt/App.scala
@@ -4,6 +4,7 @@ import com.mongodb.spark._
 import com.mongodb.spark.config.ReadConfig
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
 /**
   * @author ${user.name}
@@ -24,19 +25,37 @@ object App {
       val sc = SparkContext.getOrCreate();
       sc.setLogLevel("WARN")
 
-      // get the most viewed item for hot-items recommandations
+      // get the user journey from the database
       val dataframeParcours = MongoSpark.load(spark)
       dataframeParcours.createOrReplaceTempView("parcours")
       dataframeParcours.printSchema()
-      val top_10_most_viewed_items = spark.sql(
+
+      /*val top_10_most_viewed_items = spark.sql(
         "SELECT product_id as top_10_most_viewed_items, count(*) as nb_of_views " +
         "FROM parcours " +
         "GROUP BY product_id " +
         "ORDER BY nb_of_views DESC " +
-        "LIMIT 10").show(false)
+        "LIMIT 10").show(false)*/
+
+      // get the most viewed item for hot-items recommandations
+      var t0 = System.nanoTime()
+      val top_10_most_viewed_items = spark.sql(
+        "SELECT product_id, count(*) " +
+          "FROM parcours " +
+          "GROUP BY product_id " +
+          "ORDER BY count(*) DESC " +
+          "LIMIT 10").show(false)
+      var t1 = System.nanoTime()
+      println("Elapsed time for top_10_most_viewed_items with SQL String DataFrame : " + (t1 - t0)/1000000 + " ms\n")
+
+      t0 = System.nanoTime()
+      dataframeParcours.groupBy("product_id").count().orderBy(desc("count")).limit(10)show(false)
+      t1 = System.nanoTime()
+      println("Elapsed time for top_10_most_viewed_items with DataFrame function : " + (t1 - t0)/1000000 + " ms\n")
+
 
       // get the most viewed categories for hot-categories recommandations
-      // changing spark/mongodb config
+      // changing spark/mongodb config for collection target
       collectionName = "products"
       val dataframeProducts = MongoSpark.load(spark, ReadConfig(Map(
         "uri" -> "mongodb://127.0.0.1",
