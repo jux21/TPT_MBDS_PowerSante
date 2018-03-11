242b1e114d3dd08ce663a314fc2019233a34e764
Karl-FERRERO <karl.ferrero.kf@gmail.com>
2018-03-11 16:00:06 +0100

    pom update

--- spark_web_tpt/TPT_Predictor/src/main/java/tpt/predictor/modele/Modele.java
+++ spark_web_tpt/TPT_Predictor/src/main/java/tpt/predictor/modele/Modele.java
@@ -1,5 +1,10 @@
 package tpt.predictor.modele;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import com.mongodb.MongoClientURI;
+
 public class Modele {
 
 	public Modele() {
@@ -11,8 +16,16 @@ public class Modele {
 	 * Initialisation du modèle de données
 	 */
 	private void initModel() {
-		// TODO Auto-generated method stub
+		// JavaSparkContext jsc = createJavaSparkContext(args);
+		String uriS = "mongodb://localhost/tpt_power_sante.products";
+		MongoClientURI uri = new MongoClientURI(uriS);
 
+		SparkConf conf = new SparkConf().setMaster("local").setAppName("MongoSparkConnectorTour")
+				.set("spark.app.id", "MongoSparkConnectorTour").set("spark.mongodb.input.uri", uriS)
+				.set("spark.mongodb.output.uri", uriS);
+
+		JavaSparkContext jsc = new JavaSparkContext(conf);
+		System.out.println("hi");
 	}
 
 	/**
