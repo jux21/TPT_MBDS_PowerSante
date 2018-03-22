189c2765212cbb8bdc23aea95c2c3c57a5c1f04b
Karl-FERRERO <karl.ferrero.kf@gmail.com>
2018-03-22 18:10:04 +0100

    Load des tables MongoDB

--- spark_web_tpt/TPT_Predictor/src/main/java/tpt/predictor/modele/Modele.java
+++ spark_web_tpt/TPT_Predictor/src/main/java/tpt/predictor/modele/Modele.java
@@ -1,14 +1,25 @@
 package tpt.predictor.modele;
 
+import java.util.Set;
+
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.bson.Document;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 
 import com.mongodb.MongoClientURI;
 import com.mongodb.spark.MongoSpark;
-import com.mongodb.spark.rdd.api.java.JavaMongoRDD;
+import com.mongodb.spark.config.ReadConfig;
 
 public class Modele {
+
+	private static final String CUSTOMERS = "customers";
+	private static final String PRODUCTS = "products";
+	private static final String PARCOURS = "parcours";
+	private static final String VISITORS = "visitors";
+	private static final String COLLECTION = "collection";
+	private static final String CUSTOMERS_PATH = "mongodb://localhost/tpt_power_sante.customers";
 
 	public Modele() {
 		// Initialisation du modèle de données
@@ -19,33 +30,47 @@ public class Modele {
 	 * Initialisation du modèle de données
 	 */
 	private void initModel() {
-		// JavaSparkContext jsc = createJavaSparkContext(args);
-		String uriS = "mongodb://localhost/tpt_power_sante.products";
-		MongoClientURI uri = new MongoClientURI(uriS);
 
-		SparkConf conf = new SparkConf().setMaster("local").setAppName("MongoSparkConnectorTour")
-				.set("spark.app.id", "MongoSparkConnectorTour").set("spark.mongodb.input.uri", uriS)
-				.set("spark.mongodb.output.uri", uriS);
+		// SparkSession 
+		SparkSession spark = SparkSession.builder().master("local").appName("MongoSparkConnectorTour")
+				.config("spark.mongodb.input.uri", CUSTOMERS_PATH).config("spark.mongodb.output.uri", CUSTOMERS_PATH)
+				.getOrCreate();
 
-		JavaSparkContext jsc = new JavaSparkContext(conf);
-		JavaMongoRDD<Document> rdd = MongoSpark.load(jsc);
-		/* End Example **************************************************/
+		// Create a JavaSparkContext using the SparkSession's SparkContext object
+		JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
 
-		// Analyze data from MongoDB
-		System.out.println(rdd.count());
-		System.out.println(rdd.first().toJson());
-		
-	
+		// Load VISITORS
+		Dataset<Row> visitorsDs = MongoSpark
+				.load(jsc, ReadConfig.create(jsc).withOption(COLLECTION, VISITORS), Character.class).toDF();
+		visitorsDs.show();
+
+		// Load PARCOURS
+		Dataset<Row> parcoursDs = MongoSpark
+				.load(jsc, ReadConfig.create(jsc).withOption(COLLECTION, PARCOURS), Character.class).toDF();
+		parcoursDs.show();
+
+		// Load PRODUCTS
+		Dataset<Row> productsDs = MongoSpark
+				.load(jsc, ReadConfig.create(jsc).withOption(COLLECTION, PRODUCTS), Character.class).toDF();
+		productsDs.show();
+
+		// Load CUSTOMERS
+		Dataset<Row> customersDs = MongoSpark
+				.load(jsc, ReadConfig.create(jsc).withOption(COLLECTION, CUSTOMERS), Character.class).toDF();
+		customersDs.show();
+
+		jsc.close();
+
 	}
 
 	/**
 	 * Retourne une prédiction du parcours de l'utilisateur
 	 * 
-	 * @param params
-	 * @return
+	 * 
+	 * @param queryParams
 	 */
-	private String predict(String params) {
-		return "";
+	public void predict(Set<String> queryParams) {
+		// TODO Auto-generated method stub
 
 	}
 }
